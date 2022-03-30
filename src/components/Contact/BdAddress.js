2a4cdca20ac89e6fed7e3df34bf7f238e43349cb
asadulR <asad1252001@gmail.com.com>
2022-03-30 16:07:51 +0600

    first commit

---
+++ src/components/Contact/BdAddress.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const BdAddress = () => {
+  return (
+    <div className='border p-8 bg-blue-100 w-full'>
+      <div className='flex justify-between items-center'>
+        <div>
+          <h1>Street: 17/4 Bonani.</h1>
+          <h1>City: Dhaka</h1>
+          <h1>Phone: +8801622222000</h1>
+          <h1>Email: contact.bd@ccl.com</h1>
+        </div>
+        <div>
+          <h1 className='text-2xl'>BANGLADESH OFFICE</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BdAddress
